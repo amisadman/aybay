d56298b88b509b8e41650505fb400c63ed11ce02
amisadman <inboxsadman@gmail.com>
2025-11-05 12:56:12 +0600

    feat: implemented data operation strategy for expense

--- app/src/main/java/com/amisadman/aybaylite/strategies/ExpenseOperationStrategy.java
+++ app/src/main/java/com/amisadman/aybaylite/strategies/ExpenseOperationStrategy.java
@@ -1,4 +1,28 @@
 package com.amisadman.aybaylite.strategies;
 
-public class ExpenseOperationStrategy {
+import com.amisadman.aybaylite.Repo.DatabaseRepository;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ExpenseOperationStrategy implements DataOperationStrategy {
+    private final DatabaseRepository repository;
+
+    public ExpenseOperationStrategy(DatabaseRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public ArrayList<HashMap<String, String>> loadData() {
+        return repository.loadAllExpenses();
+    }
+
+    @Override
+    public boolean deleteData(String id) {
+        return repository.deleteExpense(id);
+    }
+
+    @Override
+    public String getType() {
+        return "EXPENSE";
+    }
 }
