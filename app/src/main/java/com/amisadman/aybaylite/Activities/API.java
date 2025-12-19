adccb0ea13fdf7a4e670567b47864ddf2fb41547
mdtahmidrahman <mdtahmidr@gmail.com>
2025-12-19 19:01:26 +0600

    feat: hide API key using buildConfigField

--- app/src/main/java/com/amisadman/aybaylite/Activities/API.java
+++ app/src/main/java/com/amisadman/aybaylite/Activities/API.java
@@ -1,17 +1,18 @@
 package com.amisadman.aybaylite.Activities;
 
+import com.amisadman.aybaylite.BuildConfig;
+
 public final class API {
     public static final String MODEL = "gemini-2.5-flash";
     public static final String BASE_URL = "https://generativelanguage.googleapis.com/v1/models/" + MODEL
             + ":generateContent";
-    private static final String API_KEY = "AIzaSyDZrXWR-gGwsHQ1lqA__esE0VHmYPrcJLM";
 
     public static String getFullUrl() {
-        return BASE_URL + "?key=" + API_KEY;
+        return BASE_URL + "?key=" + BuildConfig.GEMINI_API_KEY;
     }
 
     public static String getStreamUrl() {
         return "https://generativelanguage.googleapis.com/v1beta/models/" + MODEL + ":streamGenerateContent?key="
-                + API_KEY + "&alt=sse";
+                + BuildConfig.GEMINI_API_KEY + "&alt=sse";
     }
 }
